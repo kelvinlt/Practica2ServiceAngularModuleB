80fed7292d434ee32d82b68c6755b5f745c759a6
Kelvin <Kelvinleethen@gmail.com>
2017-10-30 13:08:50 +0100

    Mejora a parte1

--- public_html/controller.js
+++ public_html/controller.js
@@ -1,15 +1,11 @@
 angular.module('myApp', [])
         .controller('MuestraPelicula', ['$scope','peliculasService',
 function ($scope,peliculasService) {
-    $scope.algo1 = 1;
-    $scope.algo2 = 2;
-    $scope.letras = ["x","y","z"];
-    $scope.numActores =0;
-
     $scope.actoresControler = peliculasService.actores;
     
     $scope.peliculaControler = peliculasService.pelicula2;
     
     $scope.peliculasControler = peliculasService.peliculas;
     
+    $scope.numPeli = 0;
 }]);
--- public_html/service.js
+++ public_html/service.js
@@ -1,20 +1,10 @@
 angular.module('myApp').service(
         "peliculasService",function(){
-//            this.password=[]; //guardem la combinacio creada
-//            this.getCombinacion=function(){
-//              return this.password;  
-//            };
-//            this.getNumeroCombinacion=function(posicio){
-//                return this.password[posicio];
-//            };
-//            this.setCombinacion=function(pass){
-//                this.password=pass;
-//            };
             this.actores=[["Harrison","Fantastico!","12-07-1942","estadounidense"],["Mary Sean","Young","20-10-1959","estadounidense"]];  
             
-            this.pelicula=[this.actores,"Blade Runer","Ridley Scott",1982];
+            this.BladeRunner=[this.actores,"Blade Runer","Ridley Scott",1982];
             
-            this.pelicula2=[[["Hallison","Fold"],["Mark","Hamill"]],"Star Wars","George Lucas",1977];
+            this.StarWars=[[["Hallison","Fold","12-07-1942","estadounidense"],["Mark","Hamill","27-01-1978","kekistan"]],"Star Wars","George Lucas",1977];
             
-            this.peliculas=[this.pelicula,this.pelicula2];
+            this.peliculas=[this.BladeRunner,this.StarWars];
         });
